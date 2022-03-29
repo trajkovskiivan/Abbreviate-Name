dfe95c8871e5985eff5b881785884fd42d900eba
trajkovskiivan <58515706+trajkovskiivan@users.noreply.github.com>
2022-03-29 15:16:34 +0200

    Update abbreviateName.ts

--- abbreviateName.ts
+++ abbreviateName.ts
@@ -1,4 +1,3 @@
-  // Name Abbreviation Function which shows first and last name with only one letter for the middle name and disregards if Jr. or Sr. is included after the name
 function abbreviateName(name: string) {
     let [first, ...rest] = name.split(" ").length > 2 ?  name.split(" ").filter(n => n.toLowerCase() !== "senior" && n.toLowerCase() !== "junior") : name.split(" ")
     let last = rest.pop();
